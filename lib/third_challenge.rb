d45f1885a75dc930bc720b1a53ffaedede8dd005
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-23 01:07:41 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,33 +6,33 @@ def third_challenge
         age: "53"
         },
       :matriarch => {
-        name: "Lady Montague", 
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
-        name: "Romeo", 
-        age: "15", 
+        name: "Romeo",
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet", 
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
-        name: "Lady Capulet", 
+        name: "Lady Capulet",
         age: "51"
         },
       :heroine => {
-        name: "Juliet", 
-        age: "15", 
+        name: "Juliet",
+        age: "15",
         status: "alive"
         },
       :heroine_friends => []
    }
   }
 
-  
+
 end
